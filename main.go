342c0b2c25a166a40ccf2d3d87dfe58244be144a
tux <mazzu83@gmail.com>
2019-04-17 12:13:47 +0200

    testing

--- main.go
+++ main.go
@@ -7,10 +7,10 @@ import (
 	"strings"
 )
 
-var n = flag.Bool("n", false, "omit trailing newline")
-var sep = flag.String("s", " ", "separator")
-
 func main() {
+
+	var n = flag.Bool("n", false, "omit trailing newline")
+	var sep = flag.String("s", " ", "separator")
 	flag.Parse()
 	fmt.Print(strings.Join(flag.Args(), *sep))
 	if !*n {
---
+++ main_test.go
@@ -0,0 +1,17 @@
+// Echo4 prints its command-line arguments.
+package main
+
+import "testing"
+
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			main()
+		})
+	}
+}
